1aaef94d18100ec29762ce958929c5f0676e9e20
Balaaditya Patanjali <138308724+BalaadityaPatanjali@users.noreply.github.com>
2025-06-06 04:42:38 +0530

    Add Render domain to ALLOWED_HOSTS

--- log_manager/settings.py
+++ log_manager/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-replace-this-secret'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['log-auto.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'app',
